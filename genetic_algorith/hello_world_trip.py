842ac3083f46d96111416b8ecfcafbf01c81d043
offbeatheartds <offbeatheartds@gmail.com>
2026-07-18 15:08:57 +0200

    made the evaluation function

---
+++ genetic_algorith/hello_world_trip.py
@@ -0,0 +1,9 @@
+target = "hello world"
+
+def evaluations(target,solution):
+    fitness = 0
+    for letter in range(len(target)):
+        if solution[letter] == target[letter]:
+            fitness += 1
+    
+    return fitness
