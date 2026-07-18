e838a5a47415e01bee70b63fd5260f675e1924fb
offbeatheartds <offbeatheartds@gmail.com>
2026-07-18 15:55:04 +0200

    implement setup O on n*n needs improvement

--- genetic_algorith/hello_world_trip.py
+++ genetic_algorith/hello_world_trip.py
@@ -1,7 +1,26 @@
+import random
+
 target = "hello world"
+alphabet = []
+population = []
 generation = 0
 MaxGenerations = 60
 greatest_of_each_generation = []
+
+def setup(population_size,target): # specific to the problem at hand 
+    temp_population = []
+
+    for solution in range(population_size):
+        solution = ""
+        for letter in range(len(target)):
+            temp_letter = chr(random.randrange(ord('a'),ord('z')+2))
+            if temp_letter == '{':
+                temp_letter = ' '
+            solution += temp_letter
+
+        temp_population.append(solution)
+
+    return temp_population
 
 def evaluations(target,solution):
     fitness = 0
@@ -21,3 +40,4 @@ def termination(target, solution):
     # elif greatest_of_each_generation > 3:
     #     if abs()
 
+print(setup(8,target))
